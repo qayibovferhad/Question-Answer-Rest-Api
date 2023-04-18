203235fe1f8e056b0ba9ce9602c7601675883622
Ferhad <qayibovferhad04@gmail.com>
2023-04-18 23:58:55 +0400

    updated

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
-const asyncHandler = require("express-async-handler");
 const sendJwtToClient = require("../utils/authorization/sendJwtToClient");
+const asyncHandler = require("express-async-handler");
 const User = require("../models/User");
 const register = asyncHandler(async (req, res, next) => {
   const user = await new User(req.body);
